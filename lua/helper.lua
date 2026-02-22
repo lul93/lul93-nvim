29f2e74be1f18783083815b9588082afb85e64ed
h0zn <38191162+h0Zn@users.noreply.github.com>
2026-02-22 21:55:50 +0100

    helper: added debug statement

--- lua/helper.lua
+++ lua/helper.lua
@@ -1,6 +1,12 @@
 local M = {
 	size = { h = 15, v = vim.o.columns * 0.4 },
 }
+local debug = false
+local function print_debug(msg)
+	if debug then
+		print(msg)
+	end
+end
 
 function M.goto_last_terminal()
 	local state = require("state")
@@ -12,22 +18,22 @@ function M.goto_last_terminal()
 		end
 
 		if not last_terminal.tab then
-			print("last_t tab nil")
+			print_debug("last_t tab nil")
 			return
 		end
 		if not last_terminal.window then
-			print("last_t window nil")
+			print_debug("last_t window nil")
 			return
 		end
 		if not vim.api.nvim_win_is_valid(last_terminal.window) then
-			print("last_t window not valid")
+			print_debug("last_t window not valid")
 			last_terminal:open()
 			return
 		end
 
 		vim.api.nvim_set_current_tabpage(last_terminal.tab)
 		vim.api.nvim_set_current_win(last_terminal.window)
-		print("goto last terminal startinsert")
+		print_debug("goto last terminal startinsert")
 		M.start_insert()
 	end
 end
@@ -63,65 +69,65 @@ function M.goto_last_editor_window()
 	local helper = require("helper")
 	local last_editor_window = state.last_editor_window
 	if not last_editor_window.tab or not last_editor_window.win then
-		print("no last editor tab or window")
+		print_debug("no last editor tab or window")
 		return
 	end
 	if not vim.api.nvim_tabpage_is_valid(last_editor_window.tab) then
-		print("last editor tab is not valid")
+		print_debug("last editor tab is not valid")
 		return
 	end
 	if not vim.api.nvim_win_is_valid(last_editor_window.win) then
-		print("last editor window is not valid")
+		print_debug("last editor window is not valid")
 		return
 	end
 
 	if vim.api.nvim_get_current_win() == last_editor_window.win then
-		print("try fallback to last window instead of using last_editor_window")
+		print_debug("try fallback to last window instead of using last_editor_window")
 		helper.goto_prev_window()
 	else
-		print("swapping to editor window")
+		print_debug("swapping to editor window")
 		vim.api.nvim_set_current_tabpage(last_editor_window.tab)
 		vim.api.nvim_set_current_win(last_editor_window.win)
 	end
 end
 
 function M.set_last_editor_window()
-	print("Set last editor window called")
+	print_debug("Set last editor window called")
 	local state = require("state")
 	local win = vim.api.nvim_get_current_win()
 	local buf = vim.api.nvim_win_get_buf(win)
 	local last_editor_window = state.last_editor_window
 	if not vim.api.nvim_buf_is_valid(buf) then
-		print("buffer not valid")
+		print_debug("buffer not valid")
 		return
 	end
 
 	-- exclude known plugin UIs (do this early)
 	local ft = vim.bo[buf].filetype
 	if ft == "neo-tree" or ft == "toggleterm" or ft == "trouble" then
-		print("exclude known plugins")
+		print_debug("exclude known plugins")
 		return
 	end
 
 	-- must be a normal listed buffer
 	if vim.bo[buf].buftype ~= "" then
-		print("must be a normal listed buffer 1")
+		print_debug("must be a normal listed buffer 1")
 		return
 	end
 	if vim.bo[buf].buflisted ~= true then
-		print("must be a normal listed buffer 2")
+		print_debug("must be a normal listed buffer 2")
 		return
 	end
 
 	-- must have a real file name
 	local name = vim.api.nvim_buf_get_name(buf)
 	if not name or name == "" then
-		print("must have a real file name")
+		print_debug("must have a real file name")
 		return
 	end
 
 	-- all checks passed:
-	print("set last editor window")
+	print_debug("set last editor window")
 	last_editor_window.tab = vim.api.nvim_get_current_tabpage()
 	last_editor_window.win = vim.api.nvim_get_current_win()
 end
