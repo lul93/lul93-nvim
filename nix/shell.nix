40e386b0ebec8413160fd467e175068437e2d350
d4t4 <38191162+h0Zn@users.noreply.github.com>
2026-03-24 17:16:46 +0000

    format

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,11 +1,11 @@
 {
-  pkgs ?
-    import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-    }) {},
-}: let
-  plugins = import ./plugins.nix {inherit pkgs;};
-  packages = import ./packages.nix {inherit pkgs;};
+  pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  }) { },
+}:
+let
+  plugins = import ./plugins.nix { inherit pkgs; };
+  packages = import ./packages.nix { inherit pkgs; };
   nvim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
     configure = {
       customRC = ''
@@ -15,6 +15,6 @@
     };
   };
 in
-  pkgs.mkShell {
-    packages = [nvim] ++ packages;
-  }
+pkgs.mkShell {
+  packages = [ nvim ] ++ packages;
+}
