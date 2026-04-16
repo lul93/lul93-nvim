a6dc1ee429b0fe29b888a4a8144a4dc6c4a622c1
d4t4 <38191162+h0Zn@users.noreply.github.com>
2026-04-16 15:59:17 +0200

    fixed keymaps

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -25,14 +25,6 @@ keymap.set("n", "<leader>sj", function()
 	require("telescope.builtin").jumplist()
 end, { desc = "jumplist" })
 
-keymap.set("n", "<leader>ss", function()
-	local builtin = require("telescope.builtin")
-	local ok = pcall(builtin.treesitter)
-	if not ok then
-		builtin.lsp_document_symbols()
-	end
-end, { desc = "symbols" })
-
 keymap.set("n", "<leader>sg", function()
 	require("telescope.builtin").live_grep()
 end, { desc = "grep project" })
@@ -41,39 +33,47 @@ keymap.set("n", "<leader>sw", function()
 	require("telescope.builtin").grep_string()
 end, { desc = "word under cursor" })
 
-keymap.set("n", "<leader>sl", function()
-	require("telescope.builtin").buffer_fuzzy()
-end, { desc = "search current buffer" })
-
+-- last used picker
 keymap.set("n", "<leader>sp", function()
 	require("telescope.builtin").resume()
 end, { desc = "resume picker" })
+-- testing symbol index
+-- local symbols = require("config.telescope.pickers.symbol_index")
+--
+-- keymap.set("n", "<leader>ps", function()
+-- 	symbols.open({ action = "jump" })
+-- end, { desc = "Symbols: jump", { files = true, symbols = true } })
+--
+-- keymap.set("n", "<leader>py", function()
+-- 	symbols.open({ action = "yank", sources = { files = true, symbols = true } })
+-- end, { desc = "Symbols: yank" })
 
-keymap.set("n", "<leader>sa", function()
-	require("telescope.builtin").code_actions()
-end, { desc = "code actions" })
+--end testing symbol index
+-- keymap.set("n", "<leader>sl", function()
+-- 	require("telescope.builtin").buffer_fuzzy()
+-- end, { desc = "search current buffer" })
 
-keymap.set("n", "<leader>sS", function()
-	require("telescope.builtin").workspace_symbols()
-end, { desc = "workspace symbols" })
+-- keymap.set("n", "<leader>sa", function()
+-- 	require("telescope.builtin").code_actions()
+-- end, { desc = "code actions" })
+
+-- keymap.set("n", "<leader>sS", function()
+-- 	require("telescope.builtin").workspace_symbols()
+-- end, { desc = "workspace symbols" })
 
 -- rewrok with trouble?
-keymap.set("n", "<leader>gd", function()
-	require("telescope.builtin").definitions()
-end, { desc = "definitions" })
-
-keymap.set("n", "<leader>gr", function()
-	require("telescope.builtin").references()
-end, { desc = "references" })
-
-keymap.set("n", "<leader>gi", function()
-	require("telescope.builtin").implementations()
-end, { desc = "implementations" })
-
-keymap.set("n", "<leader>gt", function()
-	require("telescope.builtin").type_definitions()
-end, { desc = "type definitions" })
-
-keymap.set("n", "<leader>sd", function()
-	require("telescope.builtin").diagnostics()
-end, { desc = "diagnostics" })
+-- keymap.set("n", "<leader>gd", function()
+-- 	require("telescope.builtin").definitions()
+-- end, { desc = "definitions" })
+--
+-- keymap.set("n", "<leader>gr", function()
+-- 	require("telescope.builtin").references()
+-- end, { desc = "references" })
+--
+-- keymap.set("n", "<leader>gi", function()
+-- 	require("telescope.builtin").implementations()
+-- end, { desc = "implementations" })
+--
+-- keymap.set("n", "<leader>gt", function()
+-- 	require("telescope.builtin").type_definitions()
+-- end, { desc = "type definitions" })
